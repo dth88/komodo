4f2e2c6d7b446f077d7fedd5728e8e727dce16fc
dth88 <gdath100500@gmail.com>
2021-05-31 17:05:48 +0000

    trying native static build

---
+++ zcutil/build-aarch64-native-static.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+function cmd_pref() {
+    if type -p "$2" > /dev/null; then
+        eval "$1=$2"
+    else
+        eval "$1=$3"
+    fi
+}
+
+# If a g-prefixed version of the command exists, use it preferentially.
+function gprefix() {
+    cmd_pref "$1" "g$2" "$2"
+}
+
+gprefix READLINK readlink
+cd "$(dirname "$("$READLINK" -f "$0")")/.."
+
+# Allow user overrides to $MAKE. Typical usage for users who need it:
+#   MAKE=gmake ./zcutil/build.sh -j$(nproc)
+if [[ -z "${MAKE-}" ]]; then
+    MAKE=make
+fi
+
+# Allow overrides to $BUILD and $HOST for porters. Most users will not need it.
+#   BUILD=i686-pc-linux-gnu ./zcutil/build.sh
+if [[ -z "${BUILD-}" ]]; then
+    BUILD="$(./depends/config.guess)"
+fi
+if [[ -z "${HOST-}" ]]; then
+    HOST="$BUILD"
+fi
+
+# Allow users to set arbitrary compile flags. Most users will not need this.
+if [[ -z "${CONFIGURE_FLAGS-}" ]]; then
+    CONFIGURE_FLAGS=""
+fi
+
+eval "$MAKE" --version
+as --version
+ld -v
+
+HOST="$HOST" BUILD="$BUILD" NO_PROTON="$PROTON_ARG" "$MAKE" "$@" -C ./depends/ V=1
+./autogen.sh
+
+CONFIG_SITE="$PWD/depends/$HOST/share/config.site" CXXFLAGS="-DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread"  ./configure --build="${BUILD}" --host="${HOST}" --disable-shared --enable-static 
+
+#BUILD CCLIB
+
+WD=$PWD
+
+cd src/cc
+echo $PWD
+./makecustom_aarch64_static
+
+
+cd $WD
+
+"$MAKE" "$@" V=1 STATIC=1
