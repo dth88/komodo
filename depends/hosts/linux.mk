1ddeee9c8ee637c1771f2b28196d6b792f245781
dth88 <gdath100500@gmail.com>
2021-06-06 20:07:21 +0300

    fmul error...

--- depends/hosts/linux.mk
+++ depends/hosts/linux.mk
@@ -17,12 +17,12 @@ i686_linux_RANLIB=ranlib
 i686_linux_NM=nm
 i686_linux_STRIP=strip
 
-x86_64_linux_CC=gcc -m64
-x86_64_linux_CXX=g++ -m64
-x86_64_linux_AR=ar
-x86_64_linux_RANLIB=ranlib
-x86_64_linux_NM=nm
-x86_64_linux_STRIP=strip
+x86_64_linux_CC=x86_64-linux-gnu-gcc -m64
+x86_64_linux_CXX=x86_64-linux-gnu-g++ -m64
+x86_64_linux_AR=x86_64-linux-gnu-ar
+x86_64_linux_RANLIB=x86_64-linux-gnu-ranlib
+x86_64_linux_NM=x86_64-linux-gnu-nm
+x86_64_linux_STRIP=x86_64-linux-gnu-strip
 else
 i686_linux_CC=$(default_host_CC) -m32
 i686_linux_CXX=$(default_host_CXX) -m32
