465e4363a9b57f53bad9d4268b572c75ca8f6fe1
dth88 <gdath100500@gmail.com>
2021-06-06 08:39:53 +0300

    armv8-cross try

--- zcutil/build.sh
+++ zcutil/build.sh
@@ -99,8 +99,6 @@ ld -v
 HOST="$HOST" BUILD="$BUILD" NO_PROTON="$PROTON_ARG" "$MAKE" "$@" -C ./depends/ V=1
 ./autogen.sh
 
-CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" "$CONFIGURE_FLAGS" CXXFLAGS='-g'
-
 #BUILD CCLIB
 
 WD=$PWD
@@ -109,7 +107,8 @@ cd src/cc
 echo $PWD
 ./makecustom
 
-
 cd $WD
+
+CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure "$HARDENING_ARG" "$LCOV_ARG" "$TEST_ARG" "$MINING_ARG" "$PROTON_ARG" "$CONFIGURE_FLAGS" CXXFLAGS='-g'
 
 "$MAKE" "$@" V=1
